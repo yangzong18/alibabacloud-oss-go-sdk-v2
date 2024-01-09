7e8b20824a18a68b7ab488f87d89636822ab8517
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2024-01-09 11:46:23 +0800

    refine ecs role credential provider

--- oss/credentials/ecs_role_credentials_provider.go
+++ oss/credentials/ecs_role_credentials_provider.go
@@ -5,56 +5,66 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"path"
 	"strings"
 	"time"
 )
 
-type EcsCredentialsFetcher struct {
-	Count    int64
-	RoleName string
+const ecs_ram_cred_url = "http://100.100.100.200/latest/meta-data/ram/security-credentials/"
+
+type ecsRoleCredentialsProvider struct {
+	ramCredUrl string
+	ramRole    string
+	timeout    time.Duration
 }
 
-type EcsRoleCredentials struct {
-	AccessKeyId     string `json:"AccessKeyId,omitempty"`
-	AccessKeySecret string `json:"AccessKeySecret,omitempty"`
-	SecurityToken   string `json:"SecurityToken,omitempty"`
-	Expiration      string `json:"Expiration,omitempty"`
-	LastUpDated     string `json:"LastUpDated,omitempty"`
-	Code            string `json:"Code,omitempty"`
+type ecsRoleCredentials struct {
+	AccessKeyId     string    `json:"AccessKeyId,omitempty"`
+	AccessKeySecret string    `json:"AccessKeySecret,omitempty"`
+	SecurityToken   string    `json:"SecurityToken,omitempty"`
+	Expiration      time.Time `json:"Expiration,omitempty"`
+	LastUpDated     time.Time `json:"LastUpDated,omitempty"`
+	Code            string    `json:"Code,omitempty"`
 }
 
-func (s *EcsCredentialsFetcher) fetchEcsCredentials() (EcsRoleCredentials, error) {
-	var ecsCred EcsRoleCredentials
-	var url string
+func (p *ecsRoleCredentialsProvider) getRoleFromMetaData(ctx context.Context) (string, error) {
 	c := &http.Client{
-		Timeout: 15 * time.Second,
+		Timeout: p.timeout,
 	}
-	ecsUrlMetaData := "http://100.100.100.200/latest/meta-data/ram/security-credentials/"
-	if s.RoleName != "" {
-		url = ecsUrlMetaData + s.RoleName
-	}else{
-		resp, err := c.Get(ecsUrlMetaData)
-		if err != nil {
-			return ecsCred, err
-		}
-		if resp.StatusCode != http.StatusOK {
-			return ecsCred, errors.New("failed to fetch ecs role name")
-		}
-		defer resp.Body.Close()
-		roleName, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return ecsCred, err
-		}
-		url = ecsUrlMetaData + string(roleName)
+
+	resp, err := c.Get(p.ramCredUrl)
+	if err != nil {
+		return "", err
 	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to fetch ecs role name, resp.StatusCode:%v", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+	roleName, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if len(roleName) == 0 {
+		return "", errors.New("ecs role name is empty")
+	}
+
+	return string(roleName), nil
+}
+
+func (p *ecsRoleCredentialsProvider) getCredentialsFromMetaData(ctx context.Context) (ecsRoleCredentials, error) {
+	var ecsCred ecsRoleCredentials
+	c := &http.Client{
+		Timeout: p.timeout,
+	}
+	url := path.Join(p.ramCredUrl, p.ramRole)
 	resp, err := c.Get(url)
 	if err != nil {
 		return ecsCred, err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return ecsCred, err
 	}
@@ -62,33 +72,54 @@ func (s *EcsCredentialsFetcher) fetchEcsCredentials() (EcsRoleCredentials, error
 	if err != nil {
 		return ecsCred, err
 	}
+
 	if ecsCred.Code != "" && strings.ToUpper(ecsCred.Code) != "SUCCESS" {
-		return ecsCred, fmt.Errorf("get sts ak error,code:%s", ecsCred.Code)
+		return ecsCred, fmt.Errorf("failed to fetch credentials, return code:%s", ecsCred.Code)
 	}
 
 	if ecsCred.AccessKeyId == "" || ecsCred.AccessKeySecret == "" {
-		return ecsCred, fmt.Errorf("parsar http json body error:\n%s\n", string(body))
+		return ecsCred, fmt.Errorf("AccessKeyId or AccessKeySecret is empty, response body is '%s'", string(body))
 	}
+
 	return ecsCred, nil
 }
 
-func (s *EcsCredentialsFetcher) Fetch(ctx context.Context) (Credentials, error) {
-	credentials, err := s.fetchEcsCredentials()
-	if err != nil {
-		return Credentials{}, err
+func (p *ecsRoleCredentialsProvider) GetCredentials(ctx context.Context) (cred Credentials, err error) {
+	if len(p.ramRole) == 0 {
+		if name, err := p.getRoleFromMetaData(ctx); err != nil {
+			return cred, err
+		} else {
+			p.ramRole = name
+		}
 	}
-	expires, err := time.Parse(time.RFC3339, credentials.Expiration)
-	if err != nil {
-		return Credentials{}, err
-	}
-	s.Count++
 
-	return Credentials{
-		AccessKeyID:     credentials.AccessKeyId,
-		AccessKeySecret: credentials.AccessKeySecret,
-		SessionToken:    credentials.SecurityToken,
-		Expires:         &expires,
-	}, nil
+	ecsCred, err := p.getCredentialsFromMetaData(ctx)
+	if err != nil {
+		return cred, err
+	}
+
+	cred.AccessKeyID = ecsCred.AccessKeyId
+	cred.AccessKeySecret = ecsCred.AccessKeySecret
+	cred.SecurityToken = ecsCred.SecurityToken
+	if !ecsCred.Expiration.IsZero() {
+		cred.Expires = &ecsCred.Expiration
+	}
+
+	return cred, nil
 }
 
+func NewEcsRoleCredentialsProviderWithoutRefresh(roleNmae string) CredentialsProvider {
+	return &ecsRoleCredentialsProvider{
+		ramCredUrl: ecs_ram_cred_url,
+		ramRole:    roleNmae,
+		timeout:    15 * time.Second,
+	}
+}
 
+func NewEcsRoleCredentialsProvider(roleNmae string) CredentialsProvider {
+	p := NewEcsRoleCredentialsProviderWithoutRefresh(roleNmae)
+	provider := NewCredentialsFetcherProvider(CredentialsFetcherFunc(func(ctx context.Context) (Credentials, error) {
+		return p.GetCredentials(ctx)
+	}))
+	return provider
+}
